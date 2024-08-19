ea417d00bac29ae4708b180eb4da9a6618da6001
SoraSuegami <suegamisora@gmail.com>
2024-08-19 12:42:16 +0900

    Add more scripts for phfe

--- script/src/phfe.py
+++ script/src/phfe.py
@@ -1,21 +1,35 @@
 import sympy as sp
 import networkx as nx
-import numpy as np
+import queue
+
+# import numpy as np
 
 
-def create_graph_from_polynomials(*polynomials):
+def partial_garbling_polys(*polys):
+    graph, end_nodes = build_graph_from_polynomials(*polys)
+    adj_matrix = graph_to_adjacency_matrix(graph, end_nodes)
+    lx = build_lx_matrix(adj_matrix)
+    dfx_coeffs = build_dfx_coeffs(lx)
+    lx_bar = lx[: -len(polys), :].transpose()
+    out = {}
+    out["dfx_coeffs"] = dfx_coeffs
+    out["lx_bar"] = lx_bar
+    return out
+
+
+def build_graph_from_polynomials(*polys):
     # Initialize the graph
     graph = nx.DiGraph()
-    start_node = "Start"
+    start_node = 0
     graph.add_node(start_node, label="Start")
 
     # Initialize the node_of_mono map
-    node_of_mono = {}
     end_nodes = []
 
-    for i, poly in enumerate(polynomials):
+    for i, poly in enumerate(polys):
+        node_of_mono = {}
         # Create a unique end node for each polynomial
-        end_node = f"End_{i+1}"
+        end_node = 10000 + i
         end_nodes.append(end_node)
         graph.add_node(end_node, label="End")
 
@@ -29,66 +43,151 @@ def create_graph_from_polynomials(*polynomials):
             if len(edge_vals) == 1:
                 graph.add_edge(start_node, end_node, weight=edge_vals[0])
                 continue
-            cur_node = end_node
+            cur_node = start_node
 
             # Process edge_vals in reverse order
-            print(edge_vals)
-            for idx, edge_val in enumerate(reversed(edge_vals)):
-                print(idx)
+            for idx, edge_val in enumerate(edge_vals):
                 if idx == len(edge_vals) - 1:
                     found = False
-                    for edge in graph.out_edges(start_node, data=True):
-                        if edge[2]["weight"] == edge_val:
-                            cur_node = edge[1]
+                    for edge in graph.out_edges(cur_node, data=True):
+                        if edge[1] == end_node and edge[2]["weight"] == edge_val:
+                            cur_node = end_node
                             found = True
                             break
                     if not found:
-                        graph.add_edge(start_node, cur_node, weight=edge_val)
-                        cur_node = start_node
+                        graph.add_edge(cur_node, end_node, weight=edge_val)
+                        cur_node = end_node
                     break
 
-                # Create parent_node_vars
-                parent_node_vars = tuple(edge_vals[0 : len(edge_vals) - 1 - idx])
+                for edge in graph.out_edges(cur_node, data=True):
+                    if edge[2]["weight"] == edge_val:
+                        cur_node = edge[1]
+                        break
 
-                if parent_node_vars in node_of_mono:
-                    parent_node = node_of_mono[parent_node_vars]
-                    graph.add_edge(parent_node, cur_node, weight=edge_val)
-                    cur_node = parent_node
+                children_node_vars = tuple(edge_vals[idx + 1 :])
+                next_node = None
+                if children_node_vars in node_of_mono:
+                    next_node = node_of_mono[children_node_vars]
                 else:
-                    parent_node = len(graph.nodes)  # Create a new node
-                    graph.add_node(parent_node, label="Middle")
-                    graph.add_edge(parent_node, cur_node, weight=edge_val)
-                    cur_node = parent_node
-                    node_of_mono[parent_node_vars] = parent_node
+                    next_node = len(graph.nodes)
+                    graph.add_node(next_node, label="Middle")
+                    node_of_mono[children_node_vars] = next_node
+                graph.add_edge(cur_node, next_node, weight=edge_val)
+                cur_node = next_node
+
+                # next_node = None
+                # children_node_vars = tuple(edge_vals[idx + 1 :])
+                # if idx == len(edge_vals) - 1:
+                #     next_node = end_node
+                # elif children_node_vars in node_of_mono:
+                #     next_node = node_of_mono[children_node_vars]
+                # else:
+                #     next_node = len(graph.nodes)
+                #     graph.add_node(next_node, label="Middle")
+                #     node_of_mono[children_node_vars] = next_node
+
+                # if idx == len(edge_vals) - 1:
+                #     found = False
+                #     for edge in graph.in_edges(end_node, data=True):
+                #         if edge[2]["weight"] == edge_val:
+                #             cur_node = edge[1]
+                #             found = True
+                #             break
+                #     if not found:
+                #         graph.add_edge(start_node, cur_node, weight=edge_val)
+                #         cur_node = start_node
+                #     break
+
+                # # Create parent_node_vars
+                # parent_node_vars = tuple(edge_vals[0 : len(edge_vals) - 1 - idx])
+
+                # if parent_node_vars in node_of_mono:
+                #     parent_node = node_of_mono[parent_node_vars]
+                #     graph.add_edge(parent_node, cur_node, weight=edge_val)
+                #     cur_node = parent_node
+                # else:
+                #     parent_node = len(graph.nodes)  # Create a new node
+                #     graph.add_node(parent_node, label="Middle")
+                #     graph.add_edge(parent_node, cur_node, weight=edge_val)
+                #     cur_node = parent_node
+                #     node_of_mono[parent_node_vars] = parent_node
 
     return graph, end_nodes
 
 
 def graph_to_adjacency_matrix(graph, end_nodes):
-    # Create a mapping from nodes to indices
-    node_list = list(graph.nodes)
-    num_nodes = len(node_list)
-    adj_matrix = np.full(
-        (num_nodes, num_nodes), sp.S.Zero
-    )  # Initialize the adjacency matrix with Zero
+    num_nodes = len(graph.nodes)
+    # Initialize the adjacency matrix with Zero
+    adj_matrix = sp.Matrix(num_nodes, num_nodes, lambda i, j: sp.S.Zero)
+
+    # node_renaming = {}
+    # rename = lambda x: node_renaming[x] if x in node_renaming else x
+    # for i, end_node in enumerate(end_nodes):
+    #     print("end_node", end_node)
+    #     target_node = len(graph.nodes) - len(end_nodes) + i
+    #     print("target_node", target_node)
+    #     node_renaming[end_node] = target_node
+    #     node_renaming[target_node] = end_node
+
+    # for egde in graph.edges(data=True):
+    #     (source, target, data) = egde
+    #     if source > target:
+    #         node_renaming[source] = target
+    #         node_renaming[target] = source
+    # print(node_renaming)
+
+    node_list = sorted(list(graph.nodes))
+    print(node_list)
 
     # Iterate over the edges and populate the adjacency matrix
     for u, v, data in graph.edges(data=True):
         i = node_list.index(u)
         j = node_list.index(v)
-        print(data["weight"])
         adj_matrix[i, j] = data["weight"]
 
-    # Adjust end nodes indices
-    for i, end_node in enumerate(end_nodes):
-        end_idx = node_list.index(end_node)
-        target_idx = num_nodes - len(end_nodes) + i
-        if end_idx != target_idx:
-            adj_matrix[[end_idx, target_idx]] = adj_matrix[[target_idx, end_idx]]
-            adj_matrix[:, [end_idx, target_idx]] = adj_matrix[:, [target_idx, end_idx]]
-            node_list[end_idx], node_list[target_idx] = (
-                node_list[target_idx],
-                node_list[end_idx],
-            )
-
+    # # Adjust end nodes indices
+    # for i, end_node in enumerate(end_nodes):
+    #     end_idx = node_list.index(end_node)
+    #     target_idx = num_nodes - len(end_nodes) + i
+    #     if end_idx != target_idx:
+    #         adj_matrix.row_swap(end_idx, target_idx)
+    #         adj_matrix.col_swap(end_idx, target_idx)
+    #         node_list[end_idx], node_list[target_idx] = (
+    #             node_list[target_idx],
+    #             node_list[end_idx],
+    #         )
     return adj_matrix
+
+
+def build_lx_matrix(adj_matrix):
+    idx_matrix = sp.Matrix(
+        adj_matrix.shape[0],
+        adj_matrix.shape[1],
+        lambda i, j: sp.S.One if i == j else sp.S.Zero,
+    )
+    sp.pprint(adj_matrix)
+    lx = adj_matrix - idx_matrix
+    sp.pprint(lx)
+    lx.col_del(0)
+    # lx.row_del(-1)
+    # lx.col_del(-1)
+    sp.pprint(lx)
+    return lx
+
+
+def build_dfx_coeffs(lx):
+    num_rows = lx.rows
+    last_column = lx.cols
+    coeffs = []
+
+    for i in range(num_rows):
+        sub_matrix = lx.copy()
+        sub_matrix.row_del(i)
+        # print(sub_matrix)
+        # print(sub_matrix.shape)
+        # print(np.vectorize(lambda x: type(x))(sub_matrix))
+        det = sub_matrix.det()
+        sign = sp.Integer((-1) ** ((i + 1) * (last_column + 1)))
+        coeffs.append(sign * det)
+
+    return coeffs
--- script/tests/test_phfe.py
+++ script/tests/test_phfe.py
@@ -2,48 +2,91 @@ import sympy as sp
 import networkx as nx
 import numpy as np
 import unittest
-from src.phfe import create_graph_from_polynomials, graph_to_adjacency_matrix
+import random
+from src.phfe import (
+    build_graph_from_polynomials,
+    graph_to_adjacency_matrix,
+    partial_garbling_polys,
+)
 
 
-class TestPolynomialGraph(unittest.TestCase):
-    def test_graph_creation_and_adjacency_matrix(self):
+class TestPHFE(unittest.TestCase):
+    # def test_graph_creation_and_adjacency_matrix(self):
+    #     # Define the symbols
+    #     x1, x2, x3 = sp.symbols("x1 x2 x3")
+
+    #     # Example polynomials for testing
+    #     p1 = 3 * x1 * x2 + 5 * x2 * x3 + 7
+    #     p2 = 2 * x1 * x3 + 4 * x2 + 6
+
+    #     # Create the graph from the polynomials
+    #     graph, end_nodes = build_graph_from_polynomials(p1, p2)
+
+    #     # Convert the graph to an adjacency matrix
+    #     adj_matrix = graph_to_adjacency_matrix(graph, end_nodes)
+
+    #     # Define the expected adjacency matrix (this is an example, adjust based on actual output)
+    #     x1 = sp.symbols("x1")
+    #     x2 = sp.symbols("x2")
+    #     x3 = sp.symbols("x3")
+    #     expected_matrix = np.array(
+    #         [
+    #             [0, 2, 0, 3, 0, 5, 4, 0, 7, 6],
+    #             [0, 0, 0, 0, 0, 0, 0, x1, 0, 0],
+    #             [0, 0, 0, 0, 0, 0, 0, 0, x2, 0],
+    #             [0, 0, x1, 0, 0, 0, 0, 0, 0, 0],
+    #             [0, 0, 0, 0, 0, 0, 0, 0, x3, 0],
+    #             [0, 0, 0, 0, x2, 0, 0, 0, 0, 0],
+    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, x2],
+    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, x3],
+    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #         ]
+    #     )
+
+    #     # Test the shape of the adjacency matrix
+    #     self.assertEqual(adj_matrix.shape, (len(graph.nodes), len(graph.nodes)))
+
+    #     # Test the content of the adjacency matrix
+    #     self.assertTrue(np.array_equal(adj_matrix, expected_matrix))
+
+    def test_partial_garbling(self):
         # Define the symbols
-        x1, x2, x3 = sp.symbols("x1 x2 x3")
+        x1, x2, x3, z1, z2 = sp.symbols("x1 x2 x3 z1 z2")
 
         # Example polynomials for testing
         p1 = 3 * x1 * x2 + 5 * x2 * x3 + 7
         p2 = 2 * x1 * x3 + 4 * x2 + 6
 
-        # Create the graph from the polynomials
-        graph, end_nodes = create_graph_from_polynomials(p1, p2)
-
-        # Convert the graph to an adjacency matrix
-        adj_matrix = graph_to_adjacency_matrix(graph, end_nodes)
-
-        # Define the expected adjacency matrix (this is an example, adjust based on actual output)
-        x1 = sp.symbols("x1")
-        x2 = sp.symbols("x2")
-        x3 = sp.symbols("x3")
-        expected_matrix = np.array(
-            [
-                [0, 2, 0, 3, 0, 5, 4, 0, 7, 6],
-                [0, 0, 0, 0, 0, 0, 0, x1, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, x2, 0],
-                [0, 0, x1, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, x3, 0],
-                [0, 0, 0, 0, x2, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, x2],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, x3],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            ]
+        pgc = partial_garbling_polys(p1, p2)
+        # print(pgc)
+        lx_bar = pgc["lx_bar"]
+        print(lx_bar)
+        num_rows = lx_bar.rows
+        t = sp.Matrix([[random.randint(0, 100) for _ in range(num_rows)]])
+        t_bar = t[:, -2:]
+        # print(t)
+        # print(t_bar)
+        x1_val = sp.Integer(random.randint(0, 1))
+        x2_val = sp.Integer(random.randint(0, 1))
+        x3_val = sp.Integer(random.randint(0, 1))
+        z1_val = sp.Integer(random.randint(0, 1))
+        z2_val = sp.Integer(random.randint(0, 1))
+        z_vec = sp.Matrix([[z1_val, z2_val]])
+        lx_bar_substituted = lx_bar.subs({x1: x1_val, x2: x2_val, x3: x3_val})
+        pfx = (z_vec - t_bar).row_join(t * lx_bar_substituted)
+        # print(pfx.shape)
+        dfx_coeffs = sp.Matrix(pgc["dfx_coeffs"])
+        # print(dfx_coeffs.shape)
+        dfx = dfx_coeffs.subs({x1: x1_val, x2: x2_val, x3: x3_val})
+        # print(dfx)
+        out = pfx * dfx
+        expected_out = (
+            p1.subs({x1: x1_val, x2: x2_val, x3: x3_val}) * z1_val
+            + p2.subs({x1: x1_val, x2: x2_val, x3: x3_val}) * z2_val
         )
-
-        # Test the shape of the adjacency matrix
-        self.assertEqual(adj_matrix.shape, (len(graph.nodes), len(graph.nodes)))
-
-        # Test the content of the adjacency matrix
-        self.assertTrue(np.array_equal(adj_matrix, expected_matrix))
+        print(out)
+        print(expected_out)
 
 
 if __name__ == "__main__":
